7f34fbb3dc215958a938b26950b94f97773a3470
denniswebdel <denniswebdel@gmail.com>
2023-06-04 20:17:00 +0100

    feat: Add metadata and font imports to app layout component
    This commit adds metadata, including the page title and description, to the app layout component. It also imports four font weights of the Roboto font for use throughout the website.

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+export const metadata = {
+  title: 'Url Shortener',
+  description: 'A simple Url shortener which uses Bit.ly api',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
